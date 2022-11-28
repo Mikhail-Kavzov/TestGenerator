d8abe6fcefadb37fd989ad84888a307e9875b3a2
Mikhail-Kavzov <kavzovm@mail.ru>
2022-11-29 00:17:48 +0300

    change testGenerator signature

--- TestGenerator.Tests/GeneratorTests.cs
+++ TestGenerator.Tests/GeneratorTests.cs
@@ -1,25 +1,25 @@
 using NUnit.Framework;
 using TestGeneratorLib;
 using TestGeneratorLib.Implementation;
-using TestGeneratorLib.Interfaces;
 
 namespace TestGeneratorTests.Tests
 {
     [TestFixture]
     public class GeneratorTests
     {
-        private readonly TestGenerator _testGenerator;
+        private readonly TestGenerator _defaultTestGenerator;
+        private readonly GeneratorConfig _defaultGeneratorConfig = new();
         private const string writeFolder = @"..\..\..\..\TestGenerator\FolderToWrite\";
         private const string path = @"..\..\..\..\ClassesFolder\TestClass";
-        private readonly List<string> _files = new(12);
+        private readonly string[] _files = new string[12];
 
         public GeneratorTests()
         {
-            _testGenerator = new(new NUnitCodeTestGenerator());
-            for (int i = 0; i < _files.Capacity; i++)
+            _defaultTestGenerator = new(new NUnitCodeTestGenerator(), _defaultGeneratorConfig);
+            for (int i = 0; i < _files.Length; i++)
             {
                 var fileName = path + i.ToString() + ".cs";
-                _files.Add(fileName);
+                _files[i] = fileName;
             }
         }
 
@@ -30,7 +30,7 @@ namespace TestGeneratorTests.Tests
         public void FileNotFoundTest()
         {
             Assert.Throws<FileNotFoundException>(() =>
-            _testGenerator.Generate(new string[] { "someFile.cs" }, writeFolder));
+            _defaultTestGenerator.Generate(new string[] { "someFile.cs" }, writeFolder));
         }
 
         /// <summary>
@@ -39,8 +39,10 @@ namespace TestGeneratorTests.Tests
         [Test]
         public void CorrectTestWithOneDegree()
         {
-            var task = _testGenerator.Generate(_files.ToArray(), writeFolder, 1);
-            Assert.NotNull(task);
+            var oneGeneratorConfig = new GeneratorConfig(1, 1, 1);
+            var testOneGenerator = new TestGenerator(new NUnitCodeTestGenerator(), oneGeneratorConfig);
+            var task = testOneGenerator.Generate(_files, writeFolder);
+            Assert.That(task, Is.Not.Null);
             task.Wait();
         }
 
@@ -50,11 +52,9 @@ namespace TestGeneratorTests.Tests
         [Test]
         public void CorrectTestWithManyDegree()
         {
-            var task = _testGenerator.Generate(_files.ToArray(), writeFolder, 4);
-            Assert.NotNull(task);
+            var task = _defaultTestGenerator.Generate(_files, writeFolder);
+            Assert.That(task, Is.Not.Null);
             task.Wait();
         }
-
-
     }
 }
